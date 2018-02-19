30c695e2614d39e9050b9a8f64594bca0280bece
xiuping123 <884062869@qq.com>
2018-02-19 11:24:20 +0800

    一点点

---
+++ mz/src/components/Index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './css/index.css';
+
+export default class Home extends Component {
+	render() {
+		return(
+			<div>
+				<div className="top"></div>
+			</div>
+		)
+	}
+}
